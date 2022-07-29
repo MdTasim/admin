eca3220cbb84bc58a5de8f3a2cfe13f6ac4f9a46
MD Saidul Alam Tasim <103188938+MdTasim@users.noreply.github.com>
2022-07-29 12:09:10 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+import speech_recognition as sr
+import pyttsx3
+import datetime
+import pywhatkit
+import wikipedia
+import pyjokes
+listener = sr.Recognizer()
+alexa = pyttsx3.init()
+voice = alexa.getProperty('voices')
+alexa.setProperty('voice', voice[1].id)
+def talk(text):
+    alexa.say(text)
+    alexa.runAndWait()
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+    except:
+        pass
+    return command
+
+def run_alexa():
+    command = take_command()
+    if 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        talk('Current time is ' + time)
+    elif 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'tell me about' in command:
+        look_for = command.replace('tell me about', '')
+        info = wikipedia.summary(look_for, 1)
+        print(info)
+        talk(info)
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    elif 'date' in command:
+        talk('sorry vaiya, I have a boyfriends')
+    else:
+        talk('I did not get it but I am going to search it for you')
+        pywhatkit.search(command)
+while True:
+    run_alexa()
